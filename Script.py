0b91cb050863e3b247d5d868da7132eef948e296
Anthony Muo <a.c.muo.16@aberdeen.ac.uk>
2021-03-25 21:12:54 +0000

    Added some code

--- Script.py
+++ Script.py
@@ -14,11 +14,11 @@ def greet(who_to_greet):
     return greeting
 
 
-# print (greet('World'))
-# print (greet('Chimezie'))
+print(greet("World"))
+print(greet("Chimezie"))
 r = requests.get("https://viscom-engineering.com")
 print(r.status_code)
-
+print(r.ok)
 
 # name = input("YourName ? ")
 # print("Hello,", name)
