5155db22f910fa1ef04e06ef5418d8f838a7649f
Anthony Muo <a.c.muo.16@aberdeen.ac.uk>
2021-03-25 21:07:53 +0000

    Initial Commit

---
+++ Script.py
@@ -0,0 +1,24 @@
+### sample Script ####
+import os
+import sys
+
+# import Math
+import requests
+
+# print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "hello, {}".format(who_to_greet)
+    return greeting
+
+
+# print (greet('World'))
+# print (greet('Chimezie'))
+r = requests.get("https://viscom-engineering.com")
+print(r.status_code)
+
+
+# name = input("YourName ? ")
+# print("Hello,", name)
